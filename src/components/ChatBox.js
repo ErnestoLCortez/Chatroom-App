3b22e9c33e581774a1045bdd9678c6a26e4d0667
Louie Cortez <ernestoluiscortez@gmail.com>
2017-02-19 22:26:52 +0000

    Refactor ChatMessage.  Setup ChatBox functions.

--- src/components/ChatBox.js
+++ src/components/ChatBox.js
@@ -1,30 +1,39 @@
 import * as React from 'react';
-import AppBar from 'material-ui/AppBar';
-import {
-  List,
-  ListItem
-}
-from 'material-ui/List';
-import Avatar from 'material-ui/Avatar';
-import CommunicationChatBubble from 'material-ui/svg-icons/communication/chat-bubble';
+import List from 'material-ui/List';
+
 
 export class ChatBox extends React.Component {
+
+  constructor() {
+    super();
+  }
+
+  compontentDidMount() {
+
+  }
+
+  renderChatMessages() {
+
+  }
 
   render() {
     return (
       <List>
         <ListItem
           primaryText="Brendan Lim"
+          secondaryText="Hello."
           leftAvatar={<Avatar src="https://placehold.it/350x350" />}
           rightIcon={<CommunicationChatBubble />}
         />
         <ListItem
           primaryText="Eric Hoffman"
+          secondaryText="Sup."
           leftAvatar={<Avatar src="https://placehold.it/350x350" />}
           rightIcon={<CommunicationChatBubble />}
         />
         <ListItem
           primaryText="Grace Ng"
+          secondaryText="Fam."
           leftAvatar={<Avatar src="https://placehold.it/350x350" />}
           rightIcon={<CommunicationChatBubble />}
         />
