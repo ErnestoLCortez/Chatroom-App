bd3f2e6463f6d63d112619299739536c3e6e58f2
Louie Cortez <ernestoluiscortez@gmail.com>
2017-02-22 06:14:09 +0000

    More bug fix

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
+import {
+  Messages
+}
+from './src/data/Messages';
 
-var messages = require('./src/data/Messages');
 
 var storeMessage = function(message) {
   messages.push({
