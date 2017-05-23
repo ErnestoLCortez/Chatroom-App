3f674be18c488b9e5d3689e4f1be0e5a1ffbfb4a
Louie Cortez <ernestoluiscortez@gmail.com>
2017-05-23 16:43:11 -0700

    Switch out postgres for MongoDB

--- src/data/Messages.js
+++ src/data/Messages.js
@@ -1,28 +1,44 @@
-import Sequelize from 'sequelize';
-import {
-  sequelize
-}
-from './sequelize';
+//app/models/terasiteDB.js
 
-var Messages = sequelize.define('messages', {
-  username: {
-    type: Sequelize.STRING,
-  },
-  text: {
-    type: Sequelize.STRING
-  },
-  avatar: {
-    type: Sequelize.STRING
-  },
-  time: {
-    type: Sequelize.DATE
-  }
-}, {
-  freezeTableName: true // Model tableName will be the same as the model name
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+var SchemaTypes = Schema.Types;
+
+var MessagesSchema = new Schema({
+  username: String,
+  text: String,
+  avatar: String,
+  time: Date,
 });
 
-Messages.sync().then(function() {
-  return console.log('Message table connected');
-});
+module.exports = mongoose.model('Messages', MessagesSchema);
 
-exports.Messages = Messages;
+// import Sequelize from 'sequelize';
+// import {
+//   sequelize
+// }
+// from './sequelize';
+//
+// var Messages = sequelize.define('messages', {
+//   username: {
+//     type: Sequelize.STRING,
+//   },
+//   text: {
+//     type: Sequelize.STRING
+//   },
+//   avatar: {
+//     type: Sequelize.STRING
+//   },
+//   time: {
+//     type: Sequelize.DATE
+//   }
+// }, {
+//   freezeTableName: true // Model tableName will be the same as the model name
+// });
+//
+// Messages.sync().then(function() {
+//   return console.log('Message table connected');
+// });
+//
+// exports.Messages = Messages;
